834b652b0ca43a22b284a6fa880b321b99eb9555
denny70 <denny70@gmail.com>
2019-10-07 12:54:56 +0800

    add GUI LED control interface

---
+++ raspi_led_GUI/pyqt.py
@@ -0,0 +1,46 @@
+
+import sys
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot, QProcess
+
+import widget
+
+class Main(QWidget, widget.Ui_Widget):
+    def __init__(self):
+        super(QWidget, self).__init__()
+        self.setupUi(self)
+
+        
+        self.process = QProcess()
+
+        self.ok_btn.clicked.connect(self.sendButtonClick)
+    
+    @pyqtSlot()
+    def sendButtonClick(self):
+
+        self.gpio_port = self.GPIO_port.text()
+        self.gpio_onOff = self.on_off.text()
+        
+        self.process = QProcess()
+        cmd = str("../a.out -w " + self.gpio_port + " " +self.gpio_onOff)
+        print(self.gpio_port)
+        print(cmd)
+        self.process.start(cmd)
+        
+        
+        #self.process.start("")
+        #self.textBrowser.setText("Hello")
+    def clearButtonClick(self):
+        pass
+        #self.textBrowser.setText("")
+        
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = Main()
+    window.show()
+    app.exec_()
+    #sys.exit(app.exec_())
+        
+
---
+++ raspi_led_GUI/widget.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'widget.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        Widget.setObjectName("Widget")
+        Widget.resize(400, 300)
+        self.ok_btn = QtWidgets.QPushButton(Widget)
+        self.ok_btn.setGeometry(QtCore.QRect(200, 190, 113, 32))
+        self.ok_btn.setObjectName("ok_btn")
+        self.label = QtWidgets.QLabel(Widget)
+        self.label.setGeometry(QtCore.QRect(30, 100, 101, 21))
+        self.label.setObjectName("label")
+        self.GPIO_port = QtWidgets.QLineEdit(Widget)
+        self.GPIO_port.setGeometry(QtCore.QRect(140, 100, 61, 25))
+        self.GPIO_port.setObjectName("GPIO_port")
+        self.on_off = QtWidgets.QLineEdit(Widget)
+        self.on_off.setGeometry(QtCore.QRect(230, 100, 71, 25))
+        self.on_off.setObjectName("on_off")
+
+        self.retranslateUi(Widget)
+        QtCore.QMetaObject.connectSlotsByName(Widget)
+
+    def retranslateUi(self, Widget):
+        _translate = QtCore.QCoreApplication.translate
+        Widget.setWindowTitle(_translate("Widget", "Widget"))
+        self.ok_btn.setText(_translate("Widget", "Ok"))
+        self.label.setText(_translate("Widget", "GPIO: number"))
+        self.GPIO_port.setText(_translate("Widget", "2"))
+        self.on_off.setText(_translate("Widget", "1"))
